372b3ed08d5f1a726aca1cc4f6c1b5872a2c5ebe
kkolcz <k.kolcz@outlook.com>
2024-10-05 20:00:11 +0200

    added interface

--- NASA.Client/src/app/shared/user-decisions/user-decisions.component.ts
+++ NASA.Client/src/app/shared/user-decisions/user-decisions.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+interface IUserDecisions {
+  description: string;
+  stats: string;
+  decision1: string;
+  decision2: string;
+  decision3: string;
+  decision4: string;
+}
+
 @Component({
   selector: 'app-user-decisions',
   standalone: true,
   imports: [],
   templateUrl: './user-decisions.component.html',
-  styleUrl: './user-decisions.component.scss'
+  styleUrl: './user-decisions.component.scss',
 })
 export class UserDecisionsComponent {
-  data = {description: 'This is a user decision.', stats: 'Stats of weather', decision1: 'Decision 1', decision2: 'Decision 2', decision3: 'Decision 3', decision4: 'Decision 4'};
-
+  data: IUserDecisions = {
+    description: 'This is a user decision.',
+    stats: 'Stats of weather',
+    decision1: 'Decision 1',
+    decision2: 'Decision 2',
+    decision3: 'Decision 3',
+    decision4: 'Decision 4',
+  };
 }
