08f6c7eecf62e39056058c8d83f40cea3eba80bc
Savi-maker <schodek1234@gmail.com>
2024-10-05 16:18:11 +0200

    odzielejie maina od glownej strony

--- NASA.Client/src/app/app.component.ts
+++ NASA.Client/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { MatToolbar } from '@angular/material/toolbar';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { Router } from '@angular/router';
+import { NgModel } from '@angular/forms';
 
-import { Router } from '@angular/router'
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,14 +18,15 @@ import { Router } from '@angular/router'
     FooterComponent,
     MatToolbar,
     SpinnerComponent,
-  ],
+    HomeComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   constructor(private router: Router) {}
-goToNextPage() {
-  this.router.navigate(['/next-page']);
-}
+  goToNextPage() {
+    this.router.navigate(['/next-page']);
+  }
   title = 'NASA.Client';
 }
