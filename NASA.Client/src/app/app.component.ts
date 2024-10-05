503580e1ae765c9b7da60f3bd829cdff3b180955
Savi-maker <schodek1234@gmail.com>
2024-10-05 21:26:45 +0200

    as

--- NASA.Client/src/app/app.component.ts
+++ NASA.Client/src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { HomeComponent } from './home/home.component';
-
+import { StatisticsComponent } from './statistics/statistics.component';
 import { WorldMapComponent } from './components/world-map/world-map.component';
+import { Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'world-map', component: WorldMapComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
+
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    StatisticsComponent,
     HomeComponent,
     RouterOutlet,
     NavbarComponent,
@@ -23,6 +35,8 @@ import { WorldMapComponent } from './components/world-map/world-map.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+
+
 export class AppComponent {
   constructor(private router: Router) {}
   goToNextPage() {
--- NASA.Client/src/app/app.routes.ts
+++ NASA.Client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { WorldMapComponent } from './components/world-map/world-map.component';
 import { HomeComponent } from './home/home.component';
 import { UserDecisionsComponent } from './shared/user-decisions/user-decisions.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 export const routes: Routes = [
   {
@@ -16,4 +17,12 @@ export const routes: Routes = [
     path: 'user-decision',
     component: UserDecisionsComponent,
   },
+  {
+    path: 'statistics',
+    component:StatisticsComponent,
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+  }
 ];
