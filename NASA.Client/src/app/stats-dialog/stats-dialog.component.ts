bc4937098ee00837f569f15c51c1bef48c17e434
Savi-maker <schodek1234@gmail.com>
2024-10-06 12:47:41 +0200

    asdfg

--- NASA.Client/src/app/stats-dialog/stats-dialog.component.ts
+++ NASA.Client/src/app/stats-dialog/stats-dialog.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-stats-dialog',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, MatButtonModule,MatDialogModule,MatTooltipModule,],
   templateUrl: './stats-dialog.component.html',
   styleUrl: './stats-dialog.component.scss'
 })
